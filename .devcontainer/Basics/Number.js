53537be65a40aca2d893b68a3b3c7a5145eb30d4
Saket Karn <39449752+saketkarn@users.noreply.github.com>
2023-10-09 20:08:35 +0000

    Array

---
+++ .devcontainer/Basics/Number.js
@@ -0,0 +1,16 @@
+const a=5
+console.log(a)
+
+const b=new Number(100)
+console.log(b)
+
+console.log(typeof(b.toString()))
+
+
+// const num=123.66766
+const num=1234.578485
+console.log(num.toPrecision(3))
+
+
+const hundreds=100000000
+console.log(hundreds.toLocaleString('en-IN'))
